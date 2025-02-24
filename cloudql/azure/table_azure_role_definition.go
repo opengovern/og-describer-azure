b744a69288b0e9c1b536080ea4c92f11042976e2
Anil Chandra <120965339+acx1729@users.noreply.github.com>
2025-02-24 17:37:03 -0500

    Update table_azure_role_definition.go

--- cloudql/azure/table_azure_role_definition.go
+++ cloudql/azure/table_azure_role_definition.go
@@ -39,7 +39,7 @@ func tableAzureIamRoleDefinition(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromField("Description.RoleDefinition.ID")},
 			{
-				Name:        "type",
+				Name:        "resource_type",
 				Description: "Contains the resource type.",
 				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromField("Description.RoleDefinition.Type")},
@@ -49,7 +49,7 @@ func tableAzureIamRoleDefinition(_ context.Context) *plugin.Table {
 				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromField("Description.RoleDefinition.Properties.RoleName")},
 			{
-				Name:        "definition_type",
+				Name:        "type",
 				Description: "type of the role definition.",
 				Type:        proto.ColumnType_STRING,
 				Transform:   transform.FromField("Description.RoleDefinition.Properties.RoleType")},
