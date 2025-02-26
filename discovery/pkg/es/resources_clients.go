5f721f9b61490cf44e43fa97a7790b64eb091f81
artaasadi <arta-c@kaytu.io>
2025-02-26 16:27:38 +0100

    fix: fix subscription id

--- discovery/pkg/es/resources_clients.go
+++ discovery/pkg/es/resources_clients.go
@@ -20306,7 +20306,19 @@ func (p SubscriptionPaginator) NextPage(ctx context.Context) ([]Subscription, er
 	return values, nil
 }
 
-var listSubscriptionFilters = map[string]string{}
+var listSubscriptionFilters = map[string]string{
+	"authorization_source":      "Description.Subscription.authorizationSource",
+	"display_name":              "Description.Subscription.displayName",
+	"id":                        "Description.Subscription.id",
+	"platform_integration_id":   "IntegrationID",
+	"qualified_subscription_id": "Description.Subscription.id",
+	"state":                     "Description.Subscription.state",
+	"subscription_id":           "Description.Subscription.subscriptionId",
+	"subscription_policies":     "Description.Subscription.subscriptionPolicies",
+	"tags":                      "Description.Tags",
+	"tenant_id":                 "TenantID",
+	"title":                     "Description.Subscription.displayName",
+}
 
 func ListSubscription(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
 	plugin.Logger(ctx).Trace("ListSubscription")
